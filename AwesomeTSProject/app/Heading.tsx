54e076c496bf24ef18540ce5fd96f8ed88218358
P-iknow <apricotsoul@gmail.com>
2020-04-29 00:46:50 +0900

    feat: Heading

---
+++ AwesomeTSProject/app/Heading.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Heading() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>Todos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 80,
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 72,
+    color: 'rgba(175, 47, 47, 0.25)',
+    fontWeight: '100',
+  },
+});
