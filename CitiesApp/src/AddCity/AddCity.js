6cc064161c22ce3ca6063eee88cb3b3131d17c8d
P-iknow <apricotsoul@gmail.com>
2020-05-26 00:10:16 +0900

    feat: structure

---
+++ CitiesApp/src/AddCity/AddCity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddCity = () => {
+  return (
+    <View>
+      <Text>element</Text>
+    </View>
+  );
+};
+
+export default AddCity;
---
+++ CitiesApp/src/Cities/City.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const City = () => {
+  return (
+    <View>
+      <Text>element</Text>
+    </View>
+  );
+};
+
+export default City;
